121795a7be9e39cd246e13813d3dd19a316fb898
thomas riccardi <riccardi.thomas@gmail.com>
2010-08-23 02:00:16 +0200

    irc-log-mode: renommé en erc-view-log-mode, avec tous les changements de préfixes internes qui suivent.

--- .emacs.d/erc-view-log.el
+++ .emacs.d/erc-view-log.el
@@ -1,11 +1,11 @@
-;;; irc-log-mode.el --- Minor mode for viewing irc logs
+;;; erc-view-log.el --- Minor mode for viewing erc logs
 
 ;; Copyright (C) 2010 Antoine Levitt
 ;; Copyright (C) 2010 Thomas Riccardi
 
 ;; Author: Antoine Levitt
 ;;         Thomas Riccardi <riccardi.thomas@gmail.com>
-;; Keywords: irc erc logs colors
+;; Keywords: erc viewer logs colors
 
 ;; This program is free software. It comes without any warranty, to
 ;; the extent permitted by applicable law. You can redistribute it
@@ -17,20 +17,19 @@
 ;; Set colors on an erc log file
 
 ;;; Installation:
-;;    (require 'irc-log-mode)
-;;    (add-to-list 'auto-mode-alist '("\\.erclogs/.*\\.log" . irc-log-mode))
+;;    (require 'erc-view-log)
+;;    (add-to-list 'auto-mode-alist '("\\.erclogs/.*\\.log" . erc-view-log-mode))
 
 ;;; Options:
-;; - irc-log-nickname-face-function:
+;; - erc-view-log-nickname-face-function:
 ;;    A function that returns a face, given a nick, to colorize nicks.
 ;;    Can be nil to use standard erc face.
-;; - irc-log-my-nickname-match:
+;; - erc-view-log-my-nickname-match:
 ;;    Either a regexp or a list of nicks, to match our own nickname.
 ;;    For the list, each nick should be unique and should not contain any regexps.
 
 ;;; TODO:
 ;; - r to reload the log
-;; - rename to erc-view-log-mode
 ;; - handle priv messages?: erc-direct-msg-face
 ;; - handle nick for private messages?: erc-nick-msg-face
 ;; - handle "*** Users on"?: erc-current-nick-face
@@ -39,62 +38,62 @@
 (require 'erc)
 
 
-(defcustom irc-log-nickname-face-function
+(defcustom erc-view-log-nickname-face-function
   nil
   "A function that returns a face, given a nick. nil to use default erc face."
   :type 'function)
 
-(defcustom irc-log-my-nickname-match
+(defcustom erc-view-log-my-nickname-match
   erc-nick
   "A match for my nickname: either a regexp, or a list of nicks."
   :type '(choice (regexp :tag "A regexp that matches my nick.")
 		 (list :tag "A list of used nicks. Each nick should be unique and should not contain any regexps.")))
 
 
-(defvar irc-log-timestamp-regexp
+(defvar erc-view-log-timestamp-regexp
   ".*"
   "Regexp to match timestamps (no group match).")
 
-(defvar irc-log-nickname-regexp
+(defvar erc-view-log-nickname-regexp
   erc-valid-nick-regexp
   "Regexp to match nicknames (no group match).")
 
-(defvar irc-log-message-regexp
+(defvar erc-view-log-message-regexp
   ".*"
   "Regexp to match messages (no group match).")
 
-(defvar irc-log-notice-regexp
+(defvar erc-view-log-notice-regexp
   "\\*\\*\\* .*"
   "Regexp to match notices (no group match).")
 
-(defvar irc-log-action-regexp
+(defvar erc-view-log-action-regexp
   (format "\\* %s .*" erc-valid-nick-regexp)
   "Regexp to match actions (no group match).")
 
-(defvar irc-log-prompt-regexp
+(defvar erc-view-log-prompt-regexp
   erc-prompt
   "Regexp to match prompts (no group match).")
 
 
 (defun erc-log-nick-get-face (nick)
   "Returns a face for the given nick."
-  (if irc-log-nickname-face-function
-      (apply irc-log-nickname-face-function (list nick))
+  (if erc-view-log-nickname-face-function
+      (apply erc-view-log-nickname-face-function (list nick))
     'erc-nick-default-face))
 
 (defun erc-log-get-my-nick-regexp ()
-  "Returns a regexp that matches my nick according to custom irc-log-my-nickname-match."
-  (if (listp irc-log-my-nickname-match)
-      (regexp-opt irc-log-my-nickname-match)
-    irc-log-my-nickname-match))
+  "Returns a regexp that matches my nick according to custom erc-view-log-my-nickname-match."
+  (if (listp erc-view-log-my-nickname-match)
+      (regexp-opt erc-view-log-my-nickname-match)
+    erc-view-log-my-nickname-match))
 
 
 ;; warning: only works if erc-timestamp-format doesn't contains the pattern "<a_nickname>"
-(defun irc-log-get-keywords ()
+(defun erc-view-log-get-keywords ()
   "Returns the font-lock-defaults."
       (list
        ;; own message line
-       `(,(format "^\\(%s\\) \\(<\\)\\(%s\\)\\(>\\)[ \t]\\(%s\\)$" irc-log-timestamp-regexp (erc-log-get-my-nick-regexp) irc-log-message-regexp)
+       `(,(format "^\\(%s\\) \\(<\\)\\(%s\\)\\(>\\)[ \t]\\(%s\\)$" erc-view-log-timestamp-regexp (erc-log-get-my-nick-regexp) erc-view-log-message-regexp)
 	 (1 'erc-timestamp-face)
 	 (2 'erc-default-face)
 	 (3 'erc-my-nick-face)
@@ -102,7 +101,7 @@
 	 (5 'erc-input-face) ;; my message
 	 )
        ;; standard message line
-       `(,(format "^\\(%s\\) \\(<\\)\\(%s\\)\\(>\\)[ \t]\\(%s\\)$" irc-log-timestamp-regexp irc-log-nickname-regexp irc-log-message-regexp)
+       `(,(format "^\\(%s\\) \\(<\\)\\(%s\\)\\(>\\)[ \t]\\(%s\\)$" erc-view-log-timestamp-regexp erc-view-log-nickname-regexp erc-view-log-message-regexp)
 	 (1 'erc-timestamp-face)
 	 (2 'erc-default-face)
 	 (3 (erc-log-nick-get-face (match-string 3)))
@@ -110,17 +109,17 @@
 	 (5 'erc-default-face) ;; other message
 	 )
        ;; notice line
-       `(,(format "\\(%s\\) \\(%s\\)" irc-log-timestamp-regexp irc-log-notice-regexp)
+       `(,(format "\\(%s\\) \\(%s\\)" erc-view-log-timestamp-regexp erc-view-log-notice-regexp)
 	 (1 'erc-timestamp-face)
 	 (2 'erc-notice-face)
 	 )
        ;; action line
-       `(,(format "\\(%s\\) \\(%s\\)" irc-log-timestamp-regexp irc-log-action-regexp)
+       `(,(format "\\(%s\\) \\(%s\\)" erc-view-log-timestamp-regexp erc-view-log-action-regexp)
 	 (1 'erc-timestamp-face)
 	 (2 'erc-action-face)
 	 )
        ;; command line
-       `(,(format "\\(%s\\) \\(%s\\) \\(/.*\\)" irc-log-timestamp-regexp irc-log-prompt-regexp)
+       `(,(format "\\(%s\\) \\(%s\\) \\(/.*\\)" erc-view-log-timestamp-regexp erc-view-log-prompt-regexp)
 	 (1 'erc-timestamp-face)
 	 (2 'erc-prompt-face)
 	 (3 'erc-command-indicator-face)
@@ -129,20 +128,20 @@
 
 
 ;; undefine some syntax that's messing up with our coloring (for instance, "")
-(defvar irc-log-mode-syntax-table
+(defvar erc-view-log-mode-syntax-table
   (let ((st (make-syntax-table)))
     (modify-syntax-entry ?\" ".   " st)
     (modify-syntax-entry ?\\ ".   " st)
     st)
-  "Syntax table used while in `irc-log-mode'.")
+  "Syntax table used while in `erc-view-log-mode'.")
 
 
-(define-derived-mode irc-log-mode fundamental-mode
-  "IRC Log"
-  (setq font-lock-defaults `(,(irc-log-get-keywords)))
+(define-derived-mode erc-view-log-mode fundamental-mode
+  "ERC View Log"
+  (setq font-lock-defaults `(,(erc-view-log-get-keywords)))
   (setq buffer-read-only t))
 
 
-(provide 'irc-log-mode)
+(provide 'erc-view-log)
 
-;;; irc-log-mode.el ends here
+;;; erc-view-log.el ends here
