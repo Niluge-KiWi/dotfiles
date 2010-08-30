059f63f80b8e80b2a195db0cb208e5f9c114efdf
thomas riccardi <riccardi.thomas@gmail.com>
2010-08-30 23:35:45 +0200

    moved and update tray_daemon (used by erc)

--- .emacs.d/erc-tray-daemon/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-gcc tray_daemon.c -o tray_daemon $(pkg-config --cflags gtk+-2.0 --cflags glib-2.0 --cflags gdk-pixbuf-2.0) $(pkg-config --libs gtk+-2.0 --libs glib-2.0 --libs gdk-pixbuf-2.0) -Wall
