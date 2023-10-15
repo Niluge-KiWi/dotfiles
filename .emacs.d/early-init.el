ed600979e58ec7d7ff599db0b3b099adb74e4cab
Thomas Riccardi <riccardi.thomas@gmail.com>
2023-10-16 01:13:59 +0200

    fix early-init with emacs server/daemon mode
    - remove background-color: it's stuck witht that color when loading
      emacs from emacsclient -a '', or emacs --daemon; emacsclient
    - remove ns-*: it's only for macos, dont' car

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -32,10 +32,5 @@
                             ;; You can turn off scroll bars by uncommenting these lines:
                             ;; (vertical-scroll-bars . nil)
                             ;; (horizontal-scroll-bars . nil)
-
-                            ;; Setting the face in here prevents flashes of
-                            ;; color as the theme gets activated
-                            (background-color . "#000000")
-                            (ns-appearance . dark)
-                            (ns-transparent-titlebar . t)))
+			    ))
 
