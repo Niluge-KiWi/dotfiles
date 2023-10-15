0c48e6569e885e201cb35525fe2a2c7f34ef8f67
Thomas Riccardi <riccardi.thomas@gmail.com>
2023-10-15 20:14:06 +0200

    customize display-time
    - regroup
    - just minutes, no seconds

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,7 +37,6 @@
 (setq inhibit-splash-screen t)
 
 (setq initial-major-mode 'fundamental-mode)  ; default mode for the *scratch* buffer
-(setq display-time-default-load-average nil) ; this information is useless for most
 
 ;; Automatically reread from disk if the underlying file changes
 (setq auto-revert-interval 1)
@@ -169,8 +168,8 @@ If the new path's directories does not exist, create them."
 ;; Add the time to the tab-bar, if visible
 (add-to-list 'tab-bar-format 'tab-bar-format-align-right 'append)
 (add-to-list 'tab-bar-format 'tab-bar-format-global 'append)
-(setq display-time-format "%a %F %T")
-(setq display-time-interval 1)
+(setq display-time-format "%a %F %R")
+(setq display-time-default-load-average nil) ; this information is useless for most
 (display-time-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
