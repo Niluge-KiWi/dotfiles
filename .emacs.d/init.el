84e1ce62e551502a4f49ac246ebefd3c91e029a4
Thomas Riccardi <riccardi.thomas@gmail.com>
2023-10-18 23:28:56 +0200

    remove some extra

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -204,7 +204,6 @@ If the new path's directories does not exist, create them."
 ;; as desired
 
 ;; UI/UX enhancements mostly focused on minibuffer and autocompletion interfaces
-;; These ones are *strongly* recommended!
 (load-file (expand-file-name "extras/base.el" user-emacs-directory))
 
 ;; Packages for software development
@@ -217,11 +216,6 @@ If the new path's directories does not exist, create them."
 ;; WARNING: need to customize things inside the elisp file before use! See
 ;; the file extras/org-intro.txt for help.
 ;(load-file (expand-file-name "extras/org.el" user-emacs-directory))
-
-;; Email configuration in Emacs
-;; WARNING: needs the `mu' program installed; see the elisp file for more
-;; details.
-;(load-file (expand-file-name "extras/email.el" user-emacs-directory))
 
 ;; Tools for academic researchers
 ;(load-file (expand-file-name "extras/researcher.el" user-emacs-directory))
