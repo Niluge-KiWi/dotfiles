cf868b240164979a817ad27001015a5bfd368f40
Thomas Riccardi <riccardi.thomas@gmail.com>
2023-11-01 23:20:03 +0100

    remove cua from bedrock

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -172,9 +172,6 @@ brake whatever split of windows we might have in the frame."
 (blink-cursor-mode -1)                                ; Steady cursor
 (pixel-scroll-precision-mode)                         ; Smooth scrolling
 
-;; Use common keystrokes by default
-(cua-mode)
-
 ;; Display line numbers in programming mode
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 (setq-default display-line-numbers-width 3)           ; Set a minimum width
